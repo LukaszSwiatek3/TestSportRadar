0f37c0cc38fe93193674dfe8d424be93d4141f05
lswiatek <lukasz.swiatek2@pw.edu.pl>
2022-02-28 15:43:15 +0100

    Implement method to handle the number of expected matches given as a parameter and handling exceptions. Edit in Country Name print for competitors.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -18,24 +20,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new FileReader("C://Users/L/SportRadar/testsportradar/src/main/resources/data.json"));
         Events events = objectMapper.readValue(br, Events.class);
 
-        int i = 0;
-        int x = 10;
+        /* User choose how many matches he want to print */
+        try {
+            Scanner scanner = new Scanner(System.in);
 
-        for (Event event : events.getEvents()){
+            System.out.println("Give the number of matches you want to print: ");
+            int x = scanner.nextInt();
 
-            /* Display selected data*/
-            System.out.println("Start date: "  + event.TimeChange());
-            System.out.println(event.getCompetitors().get(0).getName() + " vs " + event.getCompetitors().get(1).getName());
-            System.out.println("Venue: " + event.VenueNameIfNotNull());
-            System.out.println(event.displayMostProbableMatchResult() + "\n");
-
-
-            /* Function response for breaking the loop */
-            if (i == x-1){
-                break;
+            if (x > events.getEvents().size()) {
+                System.out.println("The number is too big.");
             }
-            i++;
+            if (x == 0) {
+                System.out.println("There is nothing here.");
+            }
+            if (0 < x && x < events.getEvents().size()) {
+                int i = 0;
+                for (Event event : events.getEvents()) {
 
+                    /* Display selected data*/
+                    System.out.println("Start date: " + event.TimeChange());
+                    System.out.println(event.getCompetitors().get(0).getName() + " (" + event.getCompetitors().get(0).getCountry()  + ") vs " + event.getCompetitors().get(1).getName() + " (" + event.getCompetitors().get(1).getCountry() + ")");
+                    System.out.println("Venue: " + event.VenueNameIfNotNull());
+                    System.out.println(event.displayMostProbableMatchResult() + "\n");
+
+                    /* Function response for breaking the loop */
+                    if (i == x - 1) {
+                        break;
+                    }
+                    i++;
+                }
+            }
+        } catch (InputMismatchException e){
+            System.out.println("Incorrect format of input. Try to enter the number.");
         }
     }
 }
